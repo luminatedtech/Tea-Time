5eea17291bd9eb1642e2623faed82ef7e44294ed
Allen <avalmercafe02@gmail.com>
2022-09-03 16:02:00 -0400

    new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import NavBar from "./NavBar";
-
+import Home from "./Home";
+import React from "react";
+import {Route,Switch} from "react-router-dom"
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Switch>
+        <Route exact path = "/">
+          <Home/>
+        </Route>
+        <Route exact path = "/about">
+          <About/>
+        </Route>
+        <Route exact path = "/teas">
+          <Teas/>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,36 @@
 import React from "react"
 import {NavLink} from "react-router-dom"
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
 
 function NavBar() {
     return (
         <div>
             <NavLink
                 to ="/"
-                exact>
+                exact
+                style ={linkStyles}>
                     Home
             </NavLink>
             <NavLink
                 to = "/about"
                 exact
+                style ={linkStyles}
                 >
                 About
+            </NavLink>
+            <NavLink
+                to = "/teas"
+                exact
+                style = {linkStyles}>
+                    Teas
             </NavLink>
         </div>
     )
