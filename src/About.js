cf32fdad82c7be06fb5c27cecf85f316379c4d42
Allen <avalmercafe02@gmail.com>
2022-09-07 00:47:35 -0400

    some css and text

--- src/About.js
+++ src/About.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 function About () {
-    return <h1> Here, you will learn about the history about tea. </h1>
+    return <div>
+        <div id="header">
+        <h1> History of Tea </h1>
+        </div>
+        <div>
+            <p> Tea is defined as an aromatic beverage that is prepared by boiling boiled water over cured or fresh leaves of Camellia sinensis, an evergreen shrub native to East Asia. This is different from the term "herbal tea" which are drinks that are not made from this leaf. They are instead infusions of fruit, leaves or other plant parts.</p>
+            <h2>Origin of Tea</h2>
+            <p>The tea plants are native to East Asia,mainly in the southwestern parts of Asia. Before they were used to create tea, the people in East Asia actually ate tea for centuries. The first written records of tea come from China in 2737 BC. Although the earliest versions of tea were crude, throughout the centuries, a variety of techniques for processing tea and new forms were developed.</p>
+            <h2>Developments</h2>
+            <h3> Tang and Song Dynasty</h3>
+            <p>In the Tang dynasty tea was made by steaming the leaves and then pouding them into a cake form, while during the Song Dynasty, loose leaf tea was developed and became popular</p>
+            <h3>Ming Dynasty</h3>
+            <p>During the Ming Dynasty, people learned that rolling and air drying unoxidized leaves would allow the tea to remiain green and with new flavor. This would eventually become what is known today as oolong tea which is made by using partially oxidized leaves</p>
+            <h3> Spread to other Countries</h3>
+            <p>Through Western priests and merchants, tea was shipped to all parts of the European world especially to England. It was until the 18th century, where tea became more acessbile to the public, tea had really become the everyday beverage for all levels of society. Tea had become so common it had become the center of many historical events such as the Tea Act of 1773 which would eventually be the beginning of the American Revolution.</p> 
+        </div>
+        
+        
+        
+        
+        
+        </div>
 }
 
 export default About
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Home (){
-    return  <h1> Home!</h1>
-}
+    return(
+    <div id ="container">
+        <div id="header">
+            <h1 id="center"> Home!</h1>
+        </div>
+        <div id="info">
+            <p> This website is dedicated to informing newcomers about the rich history of tea and how it has become a worldwide beloved beverage</p>
+        </div>
+        <p> In addition, we also catalog different kinds of teas as well as having a form where you can submit and share your favorite tea!</p>
+    </div>
 
+)}
 export default Home
